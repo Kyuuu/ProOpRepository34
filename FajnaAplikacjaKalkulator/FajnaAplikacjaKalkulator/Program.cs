e568a2d5c45de0e169c1288aeb51db7658dcb606
Kyuuu <katarina.twardowska@gmail.com>
2018-04-19 19:16:47 +0200

    Super program

---
+++ FajnaAplikacjaKalkulator/FajnaAplikacjaKalkulator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FajnaAplikacjaKalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Console.Write("AAaaaAAAAaaaAAA");
+            } while (i > 0);
+
+        }
+    }
+}
